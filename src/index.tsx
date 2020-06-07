a908831c30114e475a1137c824a1710e0b0cd86a
pacheights <pearsonhbuck@gmail.com>
2020-06-07 14:17:05 -0700

    Fun Stats

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,63 @@ import { ConnectButton, Header } from '../library/components';
 import { TopArtistSuccessReponse } from '../library/types/spotify'
 import { openSpotifyAuth, getUsersTopArtists } from '../library/api/spotify';
 
+interface TopArtist {
+  name: string;
+  genres: string[];
+}
+
+interface TopGenres {
+  [key: string]: number;
+}
+
+type SortedGenres = [string, number][];
+
 const App = (): ReactElement => {
   const [authToken, setAuthToken] = useState('');
+  const [topArtists, setTopArtists] = useState<TopArtist[]>([]);
+  const [topGenres, setTopGenres] = useState<SortedGenres>([]);
   
   useEffect(() => {
+    setAuthToken(getAuthTokenFromURL());
+    if (!authToken) return;
+    getUsersTopArtists(authToken).then((data: TopArtistSuccessReponse) => {
+      const topArtists = data.items;
+      setTopArtists(topArtists.map(artist => ({
+        name: artist.name,
+        genres: artist.genres
+      })));
+
+      const genres: TopGenres = {};
+      topArtists.forEach(artist => {
+        artist.genres.forEach((genre: string) => {
+          if (genres[genre]) {
+            genres[genre]++
+          } else {
+            genres[genre] = 1;
+          }
+        })
+      });
+
+      const sortedGenres: SortedGenres = [];
+      for (const genre in genres) {
+        sortedGenres.push([genre, genres[genre]])
+      }
+      
+      setTopGenres(sortedGenres.sort((a, b) => b[1] - a[1]));
+    })
+  })
+
+  const getAuthTokenFromURL = (): string => {
     // URL looks like http://localhost:8080/#access_token=ABCDEF&token_type=Bearer&expires_in=3600
     const url: string = window.location.href;
     const splitUrlFromQueryParams : string[] = url.split('access_token=');
     
-    if (splitUrlFromQueryParams.length > 1) {
-      const queryParams = splitUrlFromQueryParams[1].split('&');
-      const authToken: string = queryParams[0];
+    if (splitUrlFromQueryParams.length <= 1) return '';
 
-      getUsersTopArtists(authToken)
-      .then((data: TopArtistSuccessReponse) => {
-        const topArtists = data.items;
-        const topArtistNames = topArtists.map(artist => artist.name);
-        console.log(topArtistNames);
-      })
-    }
-  })
+    const queryParams = splitUrlFromQueryParams[1].split('&');
+    const authToken: string = queryParams[0];
+    return authToken;
+  }
 
   return (
     <div className="container" style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
@@ -32,6 +69,33 @@ const App = (): ReactElement => {
       >
         Connect your Spotify account
       </ConnectButton>
+      <div style={{marginBottom: 20}}>
+        {
+          topArtists.map((artist, index) => {
+            return (
+              <p
+                key={index}
+              >
+                <strong style={{marginRight: 10}}>
+                  {index + 1}) {artist.name}
+                </strong>
+                {artist.genres.toString()}
+              </p>
+            )
+          })
+        }
+      </div>
+
+      <Header>Genres</Header>
+      <div style={{marginBottom: 20}}>
+        {
+          topGenres.map((genre, index) => (
+            <p key={index}>
+              {genre[0]}: {genre[1]}
+            </p>
+          ))
+        }
+      </div>
     </div>
   )
 }
